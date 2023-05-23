5ec23bca810d4fd9259587fb2745f5b11c5643ab
Kevin VanGelder <kevin@vangelder.tech>
2023-05-23 14:50:33 -0400

    initial commit

---
+++ app/screens/WelcomeScreen.tsx
@@ -0,0 +1,29 @@
+import { observer } from "mobx-react-lite"
+import React, { FC } from "react"
+import { View, ViewStyle } from "react-native"
+import { AppStackScreenProps } from "../navigators" // @demo remove-current-line
+import { colors } from "../theme"
+import WebView from "react-native-webview"
+
+interface WelcomeScreenProps extends AppStackScreenProps<"Welcome"> {}
+
+export const WelcomeScreen: FC<WelcomeScreenProps> = observer(function WelcomeScreen(
+  _props, // @demo remove-current-line
+) {
+
+  return (
+    <View style={$container}>
+      <WebView 
+        source={{ uri: 'https://vangelder.tech/rn-webview' }}
+        injectJSObject={{
+          authToken: 'this-is-my-token',
+        }}
+      />
+    </View>
+  )
+})
+
+const $container: ViewStyle = {
+  flex: 1,
+  backgroundColor: colors.background,
+}
